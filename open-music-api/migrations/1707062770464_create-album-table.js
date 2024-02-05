723280a345bc3703ab22b5cc339326b1b4e4a90b
Anhzf <me@anhzf.dev>
2024-02-05 23:40:01 +0700

    feat(open-music-api): :card_file_box: Create migrations

--- open-music-api/migrations/1707062770464_create-album-table.js
+++ open-music-api/migrations/1707062770464_create-album-table.js
@@ -1,7 +1,26 @@
 /* eslint-disable camelcase */
 
-/** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
-exports.up = (pgm) => { };
+const TABLE_NAME = 'albums';
 
 /** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
-exports.down = (pgm) => { };
+exports.up = (pgm) => {
+  pgm.createTable(TABLE_NAME, {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    }
+  });
+};
+
+/** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
+exports.down = (pgm) => {
+  pgm.dropTable(TABLE_NAME);
+};
--- open-music-api/migrations/1707062783553_create-song-table.js
+++ open-music-api/migrations/1707062783553_create-song-table.js
@@ -1,7 +1,44 @@
 /* eslint-disable camelcase */
 
-/** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
-exports.up = (pgm) => { };
+const TABLE_NAME = 'songs';
 
 /** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
-exports.down = (pgm) => { };
+exports.up = (pgm) => {
+  pgm.createTable(TABLE_NAME, {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INT',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INT',
+      notNull: false,
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'albums',
+      onDelete: 'CASCADE'
+    },
+  });
+};
+
+/** @param pgm {import('node-pg-migrate').MigrationBuilder}  */
+exports.down = (pgm) => {
+  pgm.dropTable(TABLE_NAME);
+};
